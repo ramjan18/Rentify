fea03c2fd94f408d935980900bdf39e1b3cab925
Ramjan <ramjannadaf487@gmail.com>
2024-10-23 18:38:52 +0530

    new update

--- public/js/map.js
+++ public/js/map.js
@@ -2,7 +2,8 @@
 	// TO MAKE THE MAP APPEAR YOU MUST
 	// ADD YOUR ACCESS TOKEN FROM
 	// https://account.mapbox.com
-	mapboxgl.accessToken = mapToken
+    require("dotenv").config();
+	mapboxgl.accessToken = process.env.MAP_TOKEN
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
